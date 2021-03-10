093754b6ccbf2b67258274434fe3f22d0a4463c2
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-03-10 11:05:36 +0100

    Overload of "plus"

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,3 +1,15 @@
-data class Color (val r : Float = 0.0F, var g : Float = 0.0F, var b : Float = 0.0F) {
+data class Color(val r: Float = 0.0F, var g: Float = 0.0F, var b: Float = 0.0F) {
+    /* RGB Color
+    params:
+        r --> Level of Red
+        g --> Level of Green
+        b --> Level of Blue
+    functions:
+        overload of operator "+" --> sum of respective params of two colors
+        is_close                 --> verifies the equality of
+     */
+    operator fun plus(other: Color): Color {
+        return Color(r + other.r, g + other.g, b + other.b)
+    }
 
 }
