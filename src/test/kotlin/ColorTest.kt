89e222e2a2d8ba46cf3b84b2274def30d1c43615
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-03-10 11:38:08 +0100

    ColorTest.kt created

---
+++ src/test/kotlin/ColorTest.kt
@@ -0,0 +1,27 @@
+import org.junit.After
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ColorTest {
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun plus() {
+    }
+
+    @Test
+    fun times() {
+    }
+
+    @Test
+    fun testTimes() {
+    }
+
+    @Test
+    fun isClose() {
+    }
+}
