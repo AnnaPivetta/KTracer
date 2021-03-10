8be18709b8393b4788c37654c336333ae04c895c
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-03-10 11:06:42 +0100

    operator times (scalar and other Color)

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,3 +1,9 @@
-data class Color (val r : Float = 0.0F, var g : Float = 0.0F, var b : Float = 0.0F) {
+data class Color(val r: Float = 0.0F, var g: Float = 0.0F, var b: Float = 0.0F) {
+    operator fun times(other: Color): Color {
+        return Color(r * other.r, g * other.g, b * other.b)
+    }
 
+    operator fun times(scalar: Float): Color {
+        return Color(r * scalar, g * scalar, b * scalar)
+    }
 }
