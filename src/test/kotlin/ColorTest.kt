e29253710e7b2cdd79659ce35b1f3e1c0f4ca4cb
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-03-10 12:00:10 +0100

    ColorTest.kt created

--- src/test/kotlin/ColorTest.kt
+++ src/test/kotlin/ColorTest.kt
@@ -14,11 +14,19 @@ class ColorTest {
     }
 
     @Test
-    fun times() {
+    fun scalarTimes() {
+        val a = Color(1.0F, 2.0F, 3.0F )
+        val b = 2.0F
+        val prod = a*b
+        assertTrue(prod.isClose(Color(2.0F, 4.0F, 6.0F)))
     }
 
     @Test
-    fun testTimes() {
+    fun colorTimes() {
+        val a = Color(1.0F, 2.0F,3.0F )
+        val b = Color(4.0F, 5.0F,6.0F )
+        val prod = a*b
+        assertTrue(prod.isClose(Color(4.0F, 10.0F, 18.0F)))
     }
 
     @Test
