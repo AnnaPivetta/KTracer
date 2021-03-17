d07f789fedd41e3f84d5b79f6ef1fc2c07d8d65c
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-03-17 11:11:37 +0100

    merged

--- src/test/kotlin/HdrImageTest.kt
+++ src/test/kotlin/HdrImageTest.kt
@@ -7,14 +7,13 @@ class HdrImageTest {
     @Test
     fun validCoordinates() {
         val img = HdrImage(7, 7)
-        assertTrue(img.validCoordinates(0,2))
+        assertTrue(img.validCoordinates(0, 2))
 
     }
-
+    @Test
     fun pixelOffset() {
         val img = HdrImage(10, 20)
-        assertTrue(img.pixelOffset(3, 6)==63)
-        assertTrue(img.pixelOffset(9, 19) == 10*20 -1) //test for the last pixel (lower right)
-
-
+        assertTrue(img.pixelOffset(3, 6) == 63)
+        assertTrue(img.pixelOffset(9, 19) == 10 * 20 - 1) //test for the last pixel (lower right)
+    }
 }
