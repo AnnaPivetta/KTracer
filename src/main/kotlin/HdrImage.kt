55b3cb23ec27dd22711c0f06e27693d69b401402
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-03-17 15:30:19 +0100

    writeFloatToStream and writePmf added

--- src/main/kotlin/HdrImage.kt
+++ src/main/kotlin/HdrImage.kt
@@ -1,3 +1,7 @@
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.nio.ByteBuffer
+
 class HdrImage(
     private val width: Int = 0,
     private val height: Int = 0,
@@ -18,6 +22,23 @@ class HdrImage(
         return pixels[pixelOffset(x,y)]
     }
 
+    fun writeFloatToStream (stream : OutputStream, value : Float ) {
+        stream.write(ByteBuffer.allocate(4).putFloat(value).array())
+    }
 
+    fun writePfm (stream: OutputStream){
+        val endianness = 1.0
+        val header = "PF\n$width $height\n$endianness\n"
+        stream.write(header.toByteArray())
+
+        for (y in height-1..0) {
+            for (x in 0..width-1) {
+                var color = getPixel(x, y)
+                writeFloatToStream(stream, color.r)
+                writeFloatToStream(stream, color.g)
+                writeFloatToStream(stream, color.b)
+            }
+        }
+    }
 }
 
