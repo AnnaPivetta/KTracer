22241b214359d639257ab0a3c71b8cc261402e13
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-03-17 10:26:42 +0100

    Class HdrImage sketch - update

--- src/main/kotlin/HdrImage.kt
+++ src/main/kotlin/HdrImage.kt
@@ -1,3 +1,3 @@
-class HdrImage (val width : Int = 0, val height: Int = 0, var pixels : Array<Color> = Array<Color>(width*height) {Color(0F, 0F, 0F)} ) {
+class HdrImage (val width : Int = 0, val height: Int = 0, var pixels : Array<Color> = Array(width*height) {Color(0F, 0F, 0F)} ) {
 
 }
