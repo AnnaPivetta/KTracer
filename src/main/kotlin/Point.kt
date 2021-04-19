5e3fb845c7979ad730539c34905767fbfd613642
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-04-19 22:06:07 +0200

    branch geometry

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,1 +1,3 @@
 class Point (var x : Float = 0.0F, var y : Float = 0.0F, var z : Float = 0.0F)
+
+//dummy comment
--- src/main/kotlin/Vector.kt
+++ src/main/kotlin/Vector.kt
@@ -1,2 +1,3 @@
 class Vector (var x: Float = 0.0F, var y: Float = 0.0F, var z: Float = 0.0F) {
 }
+//dummy comment
