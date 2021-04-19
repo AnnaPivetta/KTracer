cf6268439bcc308a7aedf9f5ceba0c50740e50d0
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-04-19 22:43:44 +0200

    Point functions implemented

---
+++ src/test/kotlin/PointTest.kt
@@ -0,0 +1,27 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PointTest {
+
+    @Test
+    fun testToString() {
+
+    }
+
+    @Test
+    fun isClose() {
+    }
+
+    @Test
+    fun plus() {
+    }
+
+    @Test
+    fun minus() {
+    }
+
+    @Test
+    fun testMinus() {
+    }
+}
