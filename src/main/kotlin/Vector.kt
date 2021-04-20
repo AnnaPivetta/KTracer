f62d0d2d349b339196c16b457a3b4e76b656fb04
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-04-20 09:22:23 +0200

    little corrections

--- src/main/kotlin/Vector.kt
+++ src/main/kotlin/Vector.kt
@@ -54,4 +54,3 @@ class Vector(var x: Float = 0.0F, var y: Float = 0.0F, var z: Float = 0.0F) {
         z /= n
     }
 }
-//dummy comment
--- src/test/kotlin/PointTest.kt
+++ src/test/kotlin/PointTest.kt
@@ -6,40 +6,40 @@ class PointTest {
 
     @Test
     fun testToString() {
-        var p = Point(1F, 2F, 3F)
+        val p = Point(1F, 2F, 3F)
         println(p.toString())
         assertTrue(p.toString()=="(1.0;2.0;3.0)")
     }
 
     @Test
     fun isClose() {
-        var a = Point(1.0F, 2.0F, 3.0F)
-        var b = Point(4.0F, 6.0F, 8.0F)
+        val a = Point(1.0F, 2.0F, 3.0F)
+        val b = Point(4.0F, 6.0F, 8.0F)
         assertTrue( a.isClose(a) )
         assertFalse( a.isClose(b) )
     }
 
     @Test
     fun plus() {
-        var a = Point(1.0F, 2.0F, 3.0F)
-        var b = Vector(4.0F, 6.0F, 8.0F)
-        var c = Point(5.0F, 8.0F, 11.0F)
+        val a = Point(1.0F, 2.0F, 3.0F)
+        val b = Vector(4.0F, 6.0F, 8.0F)
+        val c = Point(5.0F, 8.0F, 11.0F)
         assertTrue((a+b).isClose(c))
     }
 
     @Test
     fun minus() {
-        var a = Point(1.0F, 2.0F, 3.0F)
-        var b = Point(4.0F, 6.0F, 8.0F)
-        var c = Vector(-3.0F, -4.0F, -5.0F)
-       // assertTrue((a-b).isClose(c))
+        val a = Point(1.0F, 2.0F, 3.0F)
+        val b = Point(4.0F, 6.0F, 8.0F)
+        val c = Vector(-3.0F, -4.0F, -5.0F)
+        assertTrue((a-b).isClose(c))
     }
 
     @Test
     fun testMinus() {
-        var a = Point(1.0F, 2.0F, 3.0F)
-        var b = Vector(4.0F, 6.0F, 8.0F)
-        var c = Point(-3.0F, -4.0F, -5.0F)
+        val a = Point(1.0F, 2.0F, 3.0F)
+        val b = Vector(4.0F, 6.0F, 8.0F)
+        val c = Point(-3.0F, -4.0F, -5.0F)
         assertTrue((a-b).isClose(c))
 
     }
