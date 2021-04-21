59b3e3feb102a6d88a5af3c3a315e579725378ad
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-04-21 10:33:37 +0200

    Transformation and 4x4id added

---
+++ src/main/kotlin/Transformation.kt
@@ -0,0 +1,11 @@
+
+val ID4X4 = arrayOf(floatArrayOf(1.0F, 0.0F, 0.0F, 0.0F), floatArrayOf(0.0F, 1.0F, 0.0F, 0.0F),
+    floatArrayOf(0.0F, 0.0F, 1.0F, 0.0F), floatArrayOf(0.0F, 0.0F, 0.0F, 1.0F))
+
+class Transformation (
+
+    private var m  : Array<FloatArray> = ID4X4
+
+        ) {
+
+}
