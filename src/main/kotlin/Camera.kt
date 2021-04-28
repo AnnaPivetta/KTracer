5ec216c62c79729846c918bd317b57f7ce95d8ff
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-04-28 10:28:57 +0200

    Doc for Abstract Camera Class

--- src/main/kotlin/Camera.kt
+++ src/main/kotlin/Camera.kt
@@ -1,3 +1,12 @@
+/** A Camera observing the scene
+ *
+ * This class is an abstract class for implementing different kind of projection onto the screen for the
+ * observer of the scene
+ *
+ * @param d The distance of the Camera from the screen (default is 1.0)
+ * @param a The aspect ratio (width/height) of the screen (default is 1.6...MacBook standard)
+ */
+
 abstract class Camera(
     val d : Float = 1.0F,
     val a : Float = 1.6F
