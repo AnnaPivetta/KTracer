c827475618ab16b0cf4eabd008d472339b33419d
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-05-05 12:38:30 +0200

    still working on world class...

---
+++ src/main/kotlin/World.kt
@@ -0,0 +1,25 @@
+/** The collection of shapes which populate the scene
+ * This class is meant to hold a list of shapes. The shapes together make the world where the scene is set
+ */
+
+class World (
+    var shapes : MutableList<Shape> = mutableListOf<Shape>()
+        ){
+    operator fun plus( shape : Shape) {
+        shapes.add(shape)
+    }
+    fun add (shape : Shape ) {
+        shapes.add(shape)
+    }
+    /*fun rayIntersection (ray : Ray) : HitRecord? {
+       // var closest : HitRecord? =
+        for (shape in shapes) {
+            var intersection = shape.rayIntersection(ray)
+            if (intersection == null) {continue}
+
+
+        }
+
+
+    }*/
+}
