4566885315ab57c36a17e7b3b97afffdfc01a8e5
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-05-05 22:00:58 +0200

    Finished tests for rayIntersection with canonical sphere

--- src/test/kotlin/SphereTest.kt
+++ src/test/kotlin/SphereTest.kt
@@ -17,11 +17,35 @@ class SphereTest {
         val expectedNormal1 = VECZ.toNormal()
         val expectedUV1 = Vector2d(0.0F, 0.0F)
         val expectedT1 = 1.0F
-        val int = s.rayIntersection(ray1)
+        val int1 = s.rayIntersection(ray1)
 
-        assertTrue(int?.worldPoint!!.isClose(expectedHitPoint1))
-        assertTrue(int.normal.isClose(expectedNormal1))
-        assertTrue(int.t.isClose(expectedT1))
-        assertTrue(int.surfacePoint.isClose(expectedUV1))
+        assertTrue(int1?.worldPoint!!.isClose(expectedHitPoint1))
+        assertTrue(int1.normal.isClose(expectedNormal1))
+        assertTrue(int1.t.isClose(expectedT1))
+        assertTrue(int1.surfacePoint.isClose(expectedUV1))
+
+        val ray2 = Ray(origin = Point(3.0F, 0.0F, 0.0F), dir=-VECX)
+        val expectedHitPoint2 = Point(1.0F, 0.0F, 0.0F)
+        val expectedNormal2 = VECX.toNormal()
+        val expectedUV2 = Vector2d(0.0F, 0.5F)
+        val expectedT2 = 2.0F
+        val int2 = s.rayIntersection(ray2)
+
+        assertTrue(int2?.worldPoint!!.isClose(expectedHitPoint2))
+        assertTrue(int2.normal.isClose(expectedNormal2))
+        assertTrue(int2.t.isClose(expectedT2))
+        assertTrue(int2.surfacePoint.isClose(expectedUV2))
+
+        val ray3 = Ray(origin = Point(0.0F, 0.0F, 0.0F), dir=VECX)
+        val expectedHitPoint3 = Point(1.0F, 0.0F, 0.0F)
+        val expectedNormal3 = -VECX.toNormal()
+        val expectedUV3 = Vector2d(0.0F, 0.5F)
+        val expectedT3 = 1.0F
+        val int3 = s.rayIntersection(ray3)
+
+        assertTrue(int3?.worldPoint!!.isClose(expectedHitPoint3))
+        assertTrue(int3.normal.isClose(expectedNormal3))
+        assertTrue(int3.t.isClose(expectedT3))
+        assertTrue(int3.surfacePoint.isClose(expectedUV3))
     }
 }
