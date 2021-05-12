48a2682dff9bad55557438ed7b6548f75827c137
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-05-12 09:50:12 +0200

    starting with CLIKT

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.groups.groupChoice
 import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.int
@@ -7,16 +8,39 @@ import com.github.ajalt.clikt.parameters.types.int
 import java.io.FileInputStream
 import java.lang.RuntimeException
 
-sealed class LoadMode(name: String): OptionGroup(name)
+/*sealed class LoadMode(name: String): OptionGroup(name)
 class Demo : LoadMode("Demo") {
-    val mode by option(help="pmf2jpg / demo").required()
     val height by option().int()
     val width by option().int()
 }
 class Conversion : LoadMode("Conversion from pfm to jpg") {
-    val mode by option(help="pmf2jpg / demo").required()
     val fileIn by option().file().required()
     val fileOut by option().file()
+}
+class KTracer : CliktCommand () {
+    val mode by option().groupChoice(
+        "demo" to Demo(),
+        "pfm2jpg" to Conversion()
+    )
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}*/
+
+class KTracer : CliktCommand () {
+    override fun run() = Unit
+}
+class Demo : CliktCommand (name = "demo") {
+    override fun run() {
+        TODO()
+    }
+}
+class Conversion : CliktCommand (name = "pfm2ldr") {
+
+    override fun run() {
+        TODO()
+    }
 }
 
 
