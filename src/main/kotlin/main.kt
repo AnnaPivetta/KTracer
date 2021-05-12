e2d60a92ef4eff78f573c15f0bb633c7e9ad0e54
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-05-12 11:05:13 +0200

    Demo added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,9 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
 import com.github.ajalt.clikt.parameters.groups.groupChoice
 import com.github.ajalt.clikt.parameters.options.*
+import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.float
 import com.github.ajalt.clikt.parameters.types.int
 
 import java.io.FileInputStream
@@ -28,21 +30,64 @@ class KTracer : CliktCommand () {
     }
 }*/
 
-class KTracer : CliktCommand () {
+class KTracer : CliktCommand() {
     override fun run() = Unit
 }
-class Demo : CliktCommand (name = "demo") {
+
+class Demo : CliktCommand(name = "demo") {
+
+    val width by option().int().default(480)
+    val height by option().int().default(480)
+    val orthogonal by option().flag(default = false)
+    val pfmoutput by option()
+    val ldroutput by option()
+    val factor by option().float().default(0.2F)
+    val gamma by option().float().default(1.0F)
+    val format by option()
+
     override fun run() {
-        TODO()
+        //Set 10 Spheres in the World
+        val world = World()
+        val vertices = arrayOf(-0.5F, 0.5F)
+        for (x in vertices) {
+            for (y in vertices) {
+                for (z in vertices) {
+                    world.add(
+                        Sphere(
+                            T = Transformation().translation(Vector(x, y, z)) *
+                                    Transformation().scaling(Vector(0.1F, 0.1F, 0.1F))
+                        )
+                    )
+                }
+            }
+        }
+        val ar = width.toFloat() / height.toFloat()
+        val camera = if (orthogonal) OrthogonalCamera(AR = ar, T = Transformation().translation(-2.0F * VECX))
+                else PerspectiveCamera(AR = ar, T = Transformation().translation(-2.0F * VECX))
+        val im = HdrImage(width, height)
+        val computeColor: (Ray) -> Color = { if (world.rayIntersection(it) == null) BLACK else WHITE }
+        val tracer = ImageTracer(im, camera).fireAllRays(computeColor)
+
+        //Save HDR Image
+        im.saveHDRImg(pfmoutput.toString())
+        echo ("PFM Image has been saved to ${System.getProperty("user.dir")}/${pfmoutput}")
+
+        //Tone Mapping
+        echo ("Applying tone mapping with default values...")
+        im.normalizeImg(factor= factor)
+        im.clampImg()
+
+        im.saveLDRImg(ldroutput.toString(), format.toString(), gamma)
+        echo ("LDR Image has been saved to ${System.getProperty("user.dir")}/${ldroutput}")
     }
 }
-class Conversion : CliktCommand (name = "pfm2ldr") {
+
+class Conversion : CliktCommand(name = "pfm2ldr") {
 
     override fun run() {
         TODO()
     }
 }
-
 
 
 /*fun main(args:Array<String>) {
