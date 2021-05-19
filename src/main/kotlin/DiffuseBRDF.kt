91a6eafaac9f640e1afc439c348657aac665ba6f
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-05-19 11:04:10 +0200

    Updated docs for DiffuseBRDF.kt & Material.kt

--- src/main/kotlin/DiffuseBRDF.kt
+++ src/main/kotlin/DiffuseBRDF.kt
@@ -6,8 +6,9 @@ import kotlin.math.PI
  * (may be found as *Lambertian*)
  *
  * Class properties:
- * - [p] - The [Pigment] that weights the BRDF contribution given the specific point in the surface
- * - [ref] - The reflectance of the
+ * - [p] - The [Pigment] that weights the BRDF contribution given the specific point in the surface. Default is a
+ * white [UniformPigment]
+ * - [ref] - The reflectance of the material. Default is 1.0
  * @see BRDF
  */
 class DiffuseBRDF (p : Pigment = UniformPigment(WHITE.copy()), val ref : Float = 1.0F) : BRDF(p){
--- src/main/kotlin/Material.kt
+++ src/main/kotlin/Material.kt
@@ -4,8 +4,8 @@
  * Class implementing the actual material for objects
  *
  * Class properties:
- * - [brdf] - The [BRDF] of the material
- * - [emittedRad] - The emitted radiance of the material
+ * - [brdf] - The [BRDF] of the material. Default is [DiffuseBRDF]
+ * - [emittedRad] - The emitted radiance of the material. Default is a black [UniformPigment]
  */
 data class Material (val brdf: BRDF = DiffuseBRDF(), val emittedRad : Pigment = UniformPigment(BLACK)) {
 }
