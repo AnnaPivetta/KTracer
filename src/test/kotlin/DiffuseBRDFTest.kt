4503ace85348b526564074f2fe23ca8ef5f4ec79
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-05-26 16:27:41 +0200

    Test in DiffuseBRDFTest.kt

---
+++ src/test/kotlin/DiffuseBRDFTest.kt
@@ -0,0 +1,30 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+class DiffuseBRDFTest {
+
+    @Test
+    @kotlin.ExperimentalUnsignedTypes
+    fun scatterRay() {
+        val brdf = DiffuseBRDF()
+        val pcg = PCG() //Default seeds
+        val inDir = -VECZ.copy()
+        val hitPoint = Point()
+        val n = VECZ.toNormal()
+        val d = 0
+
+        val expectedR = File("./src/test/src/Rays.txt").readLines()
+            for (i in 0..10) {
+                val r = brdf.scatterRay(pcg, inDir, hitPoint, n, d)
+                assertTrue (expectedR[i] == "${r.dir.x} ${r.dir.y} ${r.dir.z}")
+
+            }
+        }
+    }
---
+++ src/test/kotlin/SpecularBRDFTest.kt
@@ -0,0 +1,11 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SpecularBRDFTest {
+
+    @Test
+    fun scatterRay() {
+
+    }
+}
