6420b072bc5c8b106fc28f30bcec3f60d8cdade7
AnnaPivetta <52953101+AnnaPivetta@users.noreply.github.com>
2021-06-07 16:52:14 +0200

    dummy files

--- build/distributions/KTracer-0.2.0/bin/Animation.zsh
+++ build/distributions/KTracer-0.2.0/bin/Animation.zsh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-for angle in $(seq 0 3); do
+#for angle in $(seq 237 359); do
     # Angle with three digits, e.g. angle="1" → angleNNN="001"
-    angleNNN=$(printf "%03d" $angle)
-    ./KTracer demo --width=640 --height=480 -n 14 --maxDepth 3 --luminosity 0.1 --angle-deg $angle --ldr-o=Animation/img$angleNNN.png --hdr-o=Animation/pfm/img$angleNNN.pfm
-done
+ #   angleNNN=$(printf "%03d" $angle)
+  #  ./KTracer demo --width=640 --height=480 -n 10 --luminosity 0.1 --angle-deg $angle --ldr-o=Animation/img$angleNNN.png --hdr-o=Animation/pfm/img$angleNNN.pfm
+#done
 
 # -r 25: Number of frames per second
 ffmpeg -r 25 -f image2 -s 640x480 -i Animation/img%03d.png \
